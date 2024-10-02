93c6e21e711d1d408e3d669ae28659b8cd7511f1
Yahya1805 <belajarkuliah902@gmail.com>
2024-10-02 23:46:35 +0700

    pertanyaan terkakhir

---
+++ L4_Usia.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class L4_Usia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //MASUKKAN INPUT
+        System.out.println("masukkan Usia: ");
+        int Usia = sc.nextInt();
+
+        
+    }
+}
